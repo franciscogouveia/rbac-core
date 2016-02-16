3a9398283bb5b94551268f9d5bb69cf629594cf7
Francisco de Gouveia <francisco@de-gouveia.eu>
2016-02-17 00:46:02 +0100

    Refactored existent tests to work with the breaking change on get. Created test for asynchronous dataretriever.

--- test/dataretriever.js
+++ test/dataretriever.js
@@ -26,9 +26,12 @@ experiment('RBAC internal modular information retrieval', () => {
 
         dataRetriever.register('test', retriever);
 
-        expect(dataRetriever.get('test:x')).to.equal('key-x');
+        dataRetriever.get('test:x', (err, result) => {
 
-        done();
+            expect(err).to.not.exist();
+            expect(result).to.equal('key-x');
+            done();
+        });
     });
 
     test('should override a valid retriever (single handler)', (done) => {
@@ -46,9 +49,14 @@ experiment('RBAC internal modular information retrieval', () => {
         dataRetriever.register('test-override', retriever1);
         dataRetriever.register('test-override', retriever2, { override: true });
 
-        expect(dataRetriever.get('test-override:test', {})).to.equal('test-2');
+        dataRetriever.get('test-override:test', (err, result) => {
 
-        done();
+            expect(err).to.not.exist();
+            expect(result).to.equal('test-2');
+
+            done();
+        });
+
     });
 
     test('should not override a valid retriever (single handler)', (done) => {
@@ -85,12 +93,32 @@ experiment('RBAC internal modular information retrieval', () => {
         dataRetriever.register(['test-override-multiple-1', 'test-override-multiple-2', 'test-override-multiple-3'], retriever1);
         dataRetriever.register(['test-override-multiple-2', 'test-override-multiple-4'], retriever2, { override: true }); // test-override-multiple-2 collides
 
-        expect(dataRetriever.get('test-override-multiple-1:test', { })).to.equal('test-1');
-        expect(dataRetriever.get('test-override-multiple-2:test', { })).to.equal('test-2');
-        expect(dataRetriever.get('test-override-multiple-3:test', { })).to.equal('test-1');
-        expect(dataRetriever.get('test-override-multiple-4:test', { })).to.equal('test-2');
+        dataRetriever.get('test-override-multiple-1:test', (err, result) => {
 
-        done();
+            expect(err).to.not.exist();
+            expect(result).to.equal('test-1');
+
+            dataRetriever.get('test-override-multiple-2:test', (err, result) => {
+
+                expect(err).to.not.exist();
+                expect(result).to.equal('test-2');
+
+
+                dataRetriever.get('test-override-multiple-3:test', (err, result) => {
+
+                    expect(err).to.not.exist();
+                    expect(result).to.equal('test-1');
+
+                    dataRetriever.get('test-override-multiple-4:test', (err, result) => {
+
+                        expect(err).to.not.exist();
+                        expect(result).to.equal('test-2');
+
+                        done();
+                    });
+                });
+            });
+        });
     });
 
     test('should not override a valid retriever (multiple handlers)', (done) => {
@@ -109,6 +137,23 @@ experiment('RBAC internal modular information retrieval', () => {
         expect(dataRetriever.register.bind(dataRetriever, ['test-override-error-multiple-2', 'test-override-error-multiple-4'], retriever2)).to.throw(Error, 'There is a data retriever already registered for the source: test-override-error-multiple-2');
 
         done();
+    });
+
+    test('should register a valid asynchronous retriever', (done) => {
+
+        const retriever = (source, key, context, callback) => {
+
+            callback(null, 'key-' + key);
+        };
+
+        dataRetriever.register('async-test', retriever);
+
+        dataRetriever.get('async-test:x', (err, result) => {
+
+            expect(err).to.not.exist();
+            expect(result).to.equal('key-x');
+            done();
+        });
     });
 
 });
